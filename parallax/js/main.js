13425e985abcbd6f20ae699d59387387ef7ad993
Protorab <50131464+Protorab@users.noreply.github.com>
2019-09-21 02:57:45 +0300

    paralax

---
+++ parallax/js/main.js
@@ -0,0 +1,14 @@
+function ShowClose(x) {
+
+  x.classList.toggle("change");
+}
+$(function () {
+  var link = $('.menu-bar');
+  var menu = $('.menu');
+  var logo = $('.logo');
+  link.on('click', function (event) {
+    event.preventDefault();
+    menu.toggleClass('_show');
+  });
+
+});

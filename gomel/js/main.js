eefe0938736ac8348d229c229175869d5971b7ca
Protorab <50131464+Protorab@users.noreply.github.com>
2019-09-23 22:30:14 +0300

    gomel

--- gomel/js/main.js
+++ gomel/js/main.js
@@ -6,9 +6,11 @@ $(function () {
   var link = $('.menu-bar');
   var menu = $('.menu');
   var logo = $('.logo');
+  var bars = $('.menu-bar>div');
   link.on('click', function (event) {
     event.preventDefault();
     menu.toggleClass('_show');
+    logo.toggleClass('color_change ');
   });
 
 });

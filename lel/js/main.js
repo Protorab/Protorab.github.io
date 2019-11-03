c3cc0e7507cc822a21acc62b22598cf0eb4f0e58
Protorab <50131464+Protorab@users.noreply.github.com>
2019-11-04 00:40:12 +0300

    order_and_thanks

--- lel/js/main.js
+++ lel/js/main.js
@@ -14,8 +14,7 @@ $(function () {
     logo.toggleClass('color_change ');
   });
 });
-// ==============
-// open modal
+// =========== open modal =====================
 $(document).ready(function () {
   $('.subtablink').click(function () {
     $('#tovari-bed> div').hide();
@@ -27,43 +26,41 @@ $(document).ready(function () {
 var show = function (state) {
   document.getElementById('modalform').style.display = state;
   document.getElementById('filter').style.display = state;
-}
+  document.getElementById('thanks-form').style.display = state;
+};
 // ====================================
 $(document).ready(function () {
-  $('.more-but').click(function () {
+  $('.more-but').click(function ShowMore() {
     $('#filter').show();
     var i = $(this).data('id');
     $('#p' + i).show();
-    // $('#p' + i).toggleClass('.info-form-grid_add');
   });
   $('.but-cancel').click(function () {
     $('.product-img-container>.product-img ').hide();
     $('.product-img-container>.product-img__default').show();
     $('#filter').hide();
     $('.info-form').hide();
-  })
+  });
   $('#filter').click(function () {
     $('.product-img-container>.product-img ').hide();
     $('.product-img-container>.product-img__default').show();
     $('#filter').hide();
     $('.info-form').hide();
-  })
-});
-// ===================================
-$(document).ready(function () {
-  $('.more-but').click(function ShowMore() {
-    // $('.info-form').hide();
-    var i = $(this).data('id');
-    $('#p' + i).show();
-    // $('#p' + i).toggleClass('.info-form-grid_add');
   });
-  $('.info-form .but-cancel').click(function () {
-    $('.info-form').hide();
-  })
 });
+// =========info form============
+// $(document).ready(function () {
+//   $('.more-but').click(function ShowMore() {
+//     var i = $(this).data('id');
+//     $('#p' + i).show();
+//   });
+//   $('.info-form .but-cancel').click(function () {
+//     $('.info-form').hide();
+//   });
+// });
 
 
-// ================================================
+// =============preview_img_in_infoform================
 $(document).ready(function () {
   $('.small-img').click(function () {
     $('.product-img-container>.product-img ').hide();
@@ -71,39 +68,72 @@ $(document).ready(function () {
     $('#b_img' + i).show();
   });
 });
-//  ===================================================
+//  ==============auto insert name of order=======================
 $(document).ready(function () {
   $('.order-but').click(function () {
     var i = $(this).data('id');
     console.log($('#p' + i + '>.product-tittle').text());
     var name = $('#p' + i + '>.product-tittle').text();
     $('#ExampleInputOrder').val(name);
-  })
-})
+  });
+});
 $(document).ready(function () {
   $('.form-but-group.order-but').click(function () {
     var i = $(this).data('id');
     console.log($('#p' + i + '>.product-tittle').text());
     var name = $('#p' + i + '>.product-tittle').text();
     $('#ExampleInputOrder').val(name);
-  })
-
-})
-// ==========
-$(window).scroll(function () {
-  if ($(window).scrollTop() > 0) {
-    $('.menu').addClass('_show');
-    console.log('11');
-  } else {
-    $('.menu').removeClass('scroll')
-  }
+  });
 });
-// =====================
+// ==============scroll menu change============================
+function scroscrollTopl() {
+  window.onscroll = function () {
+    var fixedTop = $('.menu');
+    if (window.pageYOffset > 300) {
+      fixedTop.addClass('_show');
+      this.console.log('11');
+    } else {
+      fixedTop.removeClass('_show');
+    }
+  };
+}
+// ====== color change ===========
 $(document).ready(function () {
   $('.product-img-watermark-color').hover(function () {
     // $('.main-preview-img-wrap>img ').hide();
     var i = $(this).data('id');
     $('#c' + i).toggleClass('__block__active');
     // $('#c' + i).toggleClass('__fade-in');
+  });
+});
+
+
+// ++++++++++++++++ send form +++++++++++
+$(document).ready(function () {
+
+  //E-mail Ajax Send
+  $("#modalform").submit(function () { //Change
+    var th = $(this);
+    $.ajax({
+      type: "POST",
+      url: "mail.php", //Change
+      data: th.serialize()
+    }).done(function () {
+      $('#modalform').hide();
+      $('#thanks-form').show();
+
+      // alert("Thank you!");
+      // setTimeout(function () {
+      //   // Done Functions
+      //   th.trigger("reset");
+      // }, 1000);
+    });
+    return false;
+  });
+});
+// ============thanks form close==========
+$(document).ready(function () {
+  $('#back').click(function () {
+    $('#thanks-form').hide();
   });
 });

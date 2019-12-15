618e3a923ee8ba49150d1389272ac9d632ee2135
Protorab <50131464+Protorab@users.noreply.github.com>
2019-12-15 18:34:54 +0300

    fin2

--- Invacont_Test/js/script.js
+++ Invacont_Test/js/script.js
@@ -1,10 +1,12 @@
 $(document).ready(function () {
   $('.menu-bar').click(function () {
+    $('.menu-bar').toggleClass('close');
     $('.bar').toggleClass('change');
     $('.menu-bar').toggleClass('change');
     $('.m-menu').toggleClass('__menu-show');
   })
   $('.m-menu a').click(function () {
+    $('.menu-bar').toggleClass('close');
     $('.bar').toggleClass('change');
     $('.menu-bar').toggleClass('change');
     $('.m-menu').toggleClass('__menu-show');

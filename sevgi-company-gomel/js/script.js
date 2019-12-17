42cf0c96b315e650030a96c194bd183127ffc9ac
Protorab <50131464+Protorab@users.noreply.github.com>
2019-12-18 01:25:37 +0300

    mob

--- sevgi-company-gomel/js/script.js
+++ sevgi-company-gomel/js/script.js
@@ -2,14 +2,14 @@ $(function call() {
   var trigger = $(".trigger");
   var form = $("#modalform");
   var trigger_status = $(".form-group-wrap-button p");
-  trigger.on("click", function() {
+  trigger.on("click", function () {
     form.toggleClass("full_size");
     trigger_status.toggleClass("small");
     trigger_status.toggleClass("full");
   });
 });
-$(document).ready(function() {
-  $(".more-btn").click(function() {
+$(document).ready(function () {
+  $(".more-btn").click(function () {
     var i = $(this).data("id");
     var mt = $("#mt" + i).text();
     var mi = $("#mi" + i).data("src");
@@ -24,24 +24,28 @@ $(document).ready(function() {
 });
 // ======================================================================
 // ============
-var show = function(state) {
+var show = function (state) {
   document.getElementById("inf_frm").style.display = state;
   document.getElementById("filter").style.display = state;
   // document.getElementById('thanks-form').style.display = state;
 };
 // ============
 
-$(function() {
+$(function () {
   var close = $(".close-menu");
   var link = $(".m-menu-link");
   var menu = $(".m-menu");
-  link.on("click", function(event) {
+  link.on("click", function (event) {
     event.preventDefault();
     menu.toggleClass("m-menu__active");
+    link.hide();
+    close.show();
   });
-  close.on("click", function(event) {
+  close.on("click", function (event) {
     event.preventDefault();
     menu.toggleClass("m-menu__active");
+    link.show();
+    close.hide();
   });
 });
 // ===========

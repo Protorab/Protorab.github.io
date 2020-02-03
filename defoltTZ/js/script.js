4edda67a7d89f40624edbc3acee87543d9b3f751
Protorab <50131464+Protorab@users.noreply.github.com>
2020-02-04 01:39:28 +0300

    tip_fin

--- defoltTZ/js/script.js
+++ defoltTZ/js/script.js
@@ -1,10 +1,16 @@
 $(document).ready(function () {
-  $(".menu-block-item").click(function () {
+  $("#header .menu-block-item").click(function () {
     var i = $(this).data("id");
-    $("#mb-" + i).toggleClass("_active");
-    // $("#mb-1 .arrow>path").toggleClass("_activer");
-    $("#mb-" + i + " .arrow").toggleClass("arrow_show");
-    $("#mb-" + i + " path").toggleClass("_active");
+    $(".menu-block-item").removeClass("_active");
+    $(".mb-" + i).toggleClass("_active");
+    $(".menu-block-item .arrow").removeClass("arrow_show");
+    $(".mb-" + i + " .arrow").toggleClass("arrow_show");
+    $(".menu-block-item path").removeClass("_active");
+    $(".mb-" + i + " path").toggleClass("_active");
+    $(".sub-menu-block").removeClass("_actived");
     $("#sbm-" + i).toggleClass("_actived");
+    $(".content-block img").hide();
+    $("#ci-" + i).show();
   })
+
 })

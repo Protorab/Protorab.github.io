5e95e02c452c44a712657dab91076a900a368891
Protorab <50131464+Protorab@users.noreply.github.com>
2020-02-10 01:00:14 +0300

    fin

--- TZGoodTeh/js/script.js
+++ TZGoodTeh/js/script.js
@@ -1,3 +1,34 @@
-var mySwiper = new Swiper('.swiper-container', {
-  // options here
-})
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = $(".reviews-blocks");
+  var dots = $(".dot");
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "flex";
+  dots[slideIndex - 1].className += " active";
+}
+
+$(".dot").click(function () {
+  $(this).addClass("active")
+});
